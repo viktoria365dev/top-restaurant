595b1ab5211852f0e14d6d2701de0583d21998c3
viktoria365dev <194398189+viktoria365dev@users.noreply.github.com>
2025-09-02 11:45:02 +0200

    deploy to GH pages

--- src/modules/home.js
+++ src/modules/home.js
@@ -4,7 +4,9 @@ function loadHome() {
 
   container.innerHTML = `
     <section class="hero">
-      <img src="/assets/images/hero.jpg" alt="Restaurant photo" />
+      <img src="${
+        import.meta.env.BASE_URL
+      }assets/images/hero.jpg" alt="Restaurant photo" />
       <div class="hero-text">
         <h2>Welcome to the Italiano Bello Restaurant</h2>
         <p>Where every bite feels like home.</p>
